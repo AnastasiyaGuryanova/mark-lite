d7ea005c53ca002c3d83b083a03697bba8b62498
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-10-20 12:04:30 +0300

    feat: добавлены переиспользуемые валидаторы для форм аутентификации

--- src/features/auth/ui/SigninForm/SigninForm.tsx
+++ src/features/auth/ui/SigninForm/SigninForm.tsx
@@ -5,6 +5,7 @@ import { IconAt } from '@tabler/icons-react';
 import { useForm } from '@mantine/form';
 import { SigninFormProps } from './SigninForm.types';
 import { Button } from 'Shared/ui';
+import { emailValidator, passwordValidator } from 'Features/auth';
 
 export const SigninForm: FC<SigninFormProps> = ({
 	onSubmit,
@@ -19,10 +20,8 @@ export const SigninForm: FC<SigninFormProps> = ({
 			password: '',
 		},
 		validate: {
-			email: (value) =>
-				/^\S+@\S+\.\S+$/.test(value) ? null : 'Введите корректный email.',
-			password: (value) =>
-				value.length >= 6 ? null : 'Пароль должен быть не менее 6 символов.',
+			email: emailValidator,
+			password: passwordValidator,
 		},
 	});
 
